690f3b368d582866a785a1786773cc289766c3de
pankajrj <56971850+pankajrj@users.noreply.github.com>
2022-01-30 10:47:04 +0530

    Update test_run.sh

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker container run -p 80:80 -d --name webserver pankajrj24/webserver
+docker container run -p 80:80 -d --name webserver pankaj24/webserver
 echo 'Access website at http://<public_ip>'
